f295d1d73f8e69db841823114233b00626fe3877
evanlin-dev <evanlin2912@gmail.com>
2024-02-18 18:25:02 -0500

    Added login and register page

---
+++ src/Register.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './Register.css'
+import { useNavigate } from 'react-router-dom';
+
+function Register() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Password Manager</h1>
+      <div className="card">
+        <div className="card-header">
+          <button className="header-btn" onClick={() => navigate("/login")}>Login</button>
+          <div className="separator"></div>
+          <button className="header-btn" style={{textDecoration:"underline"}} onClick={() => navigate("/register")}>Register</button>
+        </div>
+        <input name="username" className="input-field-login" placeholder="Username"/>
+        <input name="password" className="input-field-password" placeholder="Password"/>
+        <div className="forgot-pw-separator-1"></div>
+        <button className="forgot-btn">Forgot your password?</button>
+        <div className="forgot-pw-separator-2"></div>
+        <button className="auth-btn">Login</button>
+      </div>
+    </>
+  )
+}
+
+export default Register
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Login from './Login.jsx'
+import Register from './Register.jsx'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
 )
